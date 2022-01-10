2c2bda4cfba264b57ad656c7054b1d99c0c30204
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-01-11 02:50:40 +0600

    style: add colors to info messages

--- setup_scripts/brew.sh
+++ setup_scripts/brew.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# Install Homebrew
 
+COLOR_REST="$(tput sgr0)"
+COLOR_GREEN="$(tput setaf 14)"
+
+printf '%s%s%s\n' $COLOR_GREEN 'Installing homebrew...' $COLOR_REST
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update
 brew upgrade
 
 # Install packages
-
+printf '%s%s%s\n' $COLOR_GREEN 'Installing brew packages...' $COLOR_REST
 apps=(
     cmake
     coreutils
@@ -45,11 +48,11 @@ apps=(
 brew install "${apps[@]}"
 
 
-Install cask packages
-
-# google-chrome
+# Install cask packages
+printf '%s%s%s\n' $COLOR_GREEN 'Installing brew cask packages...' $COLOR_REST
 cask_apps=(
     raycast
+    google-chrome
     iterm2
     visual-studio-code
     postman
@@ -65,10 +68,8 @@ cask_apps=(
     rectangle
 )
 
-# brew install "${cask_apps[@]}" --cask
-
 for item in "${cask_apps[@]}"; do
   brew info "${item}" --cask | grep --quiet 'Not installed' && brew install "${item}" --cask
 done
 
-echo '################# Brew Install Done #################'
+printf '%s%s%s\n' $COLOR_GREEN '################# Brew Install Done #################' $COLOR_REST
