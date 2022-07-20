3ffc1b17befaa5de2ab32f8868faf2f4ec73fe76
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-07-20 16:37:44 +0600

    feat: add fisher fish plugin manager

---
+++ setup_scripts/fisher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install fisher fish plugin manager
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+# Install plugins
+fisher install oh-my-fish/plugin-jump
+fisher install jorgebucaran/autopair.fish
+fisher install jorgebucaran/replay.fish
+fisher install PatrickF1/fzf.fish
+fisher install jethrokuan/z
+fisher install markcial/upto
+fisher install franciscolourenco/done
