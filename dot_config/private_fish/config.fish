1ddd328e81617e464f1e42aa4f5ace283f676a95
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-07-20 17:36:55 +0600

    feat: add fzf config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,7 @@ set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
 starship init fish | source
+fzf_configure_bindings --git_log=\cg --directory=\cf
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '$HOME/google-cloud-sdk/path.fish.inc' ]; . '$HOME/google-cloud-sdk/path.fish.inc'; end
