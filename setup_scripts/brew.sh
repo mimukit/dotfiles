1381288115a47e91ee710c0eb8ae4f1a18def7ca
Mukitul Islam Mukit <hellomukit@gmail.com>
2022-07-21 23:44:46 +0600

    feat: add separate script for brew install

--- setup_scripts/brew.sh
+++ setup_scripts/brew.sh
@@ -3,9 +3,7 @@
 COLOR_REST="$(tput sgr0)"
 COLOR_GREEN="$(tput setaf 14)"
 
-printf '%s%s%s\n' $COLOR_GREEN 'Installing homebrew...' $COLOR_REST
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+printf '%s%s%s\n' $COLOR_GREEN 'Updating homebrew...' $COLOR_REST
 brew update
 brew upgrade
 
