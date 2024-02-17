d3672eba32483c38fff121f9d1bbf6530b36a2e0
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-02-18 01:41:00 +0600

    feat: add color and editor variables to fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -9,6 +9,13 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 set -gx PATH "/opt/homebrew/bin" $PATH
 set -gx EDITOR "nvim"
 
+# Colors for ls command
+set -gx LSCOLORS "Cxbgdxdxbxdgeghegeacad"
+
+# Editor
+set -x EDITOR nvim
+set -x GIT_EDITOR $EDITOR
+
 starship init fish | source
 fzf_configure_bindings --git_log=\cg --directory=\cf
 
