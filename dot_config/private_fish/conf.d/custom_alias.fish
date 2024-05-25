c31725247d642a383543fbcf6e2bbb6d76bc3ed6
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-05-25 19:40:26 +0600

    chore: update fish alias

--- dot_config/private_fish/conf.d/custom_alias.fish
+++ dot_config/private_fish/conf.d/custom_alias.fish
@@ -1,4 +1,4 @@
 # Alias List
 alias ls 'exa --long --icons --header --no-user --sort=type'
-alias lsa 'exa --long --icons --header --no-user --sort=type --all'
+alias la 'exa --long --icons --header --no-user --sort=type --all'
 alias actm 'act --container-architecture linux/amd64 -r --secret-file act.secrets'
