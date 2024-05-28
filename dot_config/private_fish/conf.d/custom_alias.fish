fe7002ea5daee2f760a3fcb148d7a675fab2f825
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-05-29 01:55:32 +0600

    chore: add alias for rm

--- dot_config/private_fish/conf.d/custom_alias.fish
+++ dot_config/private_fish/conf.d/custom_alias.fish
@@ -2,3 +2,4 @@
 alias ls 'exa --long --icons --header --no-user --sort=type'
 alias la 'exa --long --icons --header --no-user --sort=type --all'
 alias actm 'act --container-architecture linux/amd64 -r --secret-file act.secrets'
+alias rm 'trash'
