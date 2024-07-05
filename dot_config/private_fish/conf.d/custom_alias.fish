74371595f085968565112c8ff179a3adefbb674c
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-05 13:56:13 +0600

    chore: add alias for localias

--- dot_config/private_fish/conf.d/custom_alias.fish
+++ dot_config/private_fish/conf.d/custom_alias.fish
@@ -2,6 +2,7 @@
 alias actm 'act --container-architecture linux/amd64 -r --secret-file act.secrets'
 alias ls 'exa --long --icons --header --no-user --sort=type'
 alias la 'exa --long --icons --header --no-user --sort=type --all'
+alias las 'localias'
 alias ld 'lazydocker'
 alias lg 'lazygit'
 alias rm 'trash'
