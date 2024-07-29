a5fef1deabb8d27b915ea71fe55a02b055744cb1
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-07-29 15:28:14 +0600

    chore: add fish alias for custom find

--- dot_config/private_fish/conf.d/custom_alias.fish
+++ dot_config/private_fish/conf.d/custom_alias.fish
@@ -1,5 +1,6 @@
 # Alias List
 alias actm 'act --container-architecture linux/amd64 -r --secret-file act.secrets'
+alias ff 'fd -t f | fzf --preview "bat --color=always --style=numbers {}"'
 alias ls 'eza --long --icons --header --no-user --sort=type'
 alias la 'eza --long --icons --header --no-user --sort=type --all'
 alias las 'localias'
