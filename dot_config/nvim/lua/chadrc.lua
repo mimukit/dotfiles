a31ffb2d88cac0c377e0cab8c352ec2ec1e7f9ff
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-03 02:47:18 +0600

    style: fix formatting

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -3,34 +3,34 @@
 
 ---@type ChadrcConfig
 local M = {
-  base46 = {
-    theme = "catppuccin",
-    transparency = true,
-  },
+	base46 = {
+		theme = "catppuccin",
+		transparency = true,
+	},
 
-  ui = {
-    statusline = {
-      theme = "minimal",
-      separator_style = "round",
-    },
+	ui = {
+		statusline = {
+			theme = "minimal",
+			separator_style = "round",
+		},
 
-    nvdash = {
-      load_on_startup = true,
-    },
-  },
+		nvdash = {
+			load_on_startup = true,
+		},
+	},
 
-  term = {
-    winopts = { number = false, relativenumber = false },
-    sizes = { sp = 0.3, vsp = 0.4, ["bo sp"] = 0.3, ["bo vsp"] = 0.4 },
-    float = {
-      relative = "editor",
-      row = 0.1,
-      col = 0.2,
-      width = 0.7,
-      height = 0.6,
-      border = "single",
-    },
-  },
+	term = {
+		winopts = { number = false, relativenumber = false },
+		sizes = { sp = 0.3, vsp = 0.4, ["bo sp"] = 0.3, ["bo vsp"] = 0.4 },
+		float = {
+			relative = "editor",
+			row = 0.1,
+			col = 0.2,
+			width = 0.7,
+			height = 0.6,
+			border = "single",
+		},
+	},
 }
 
 return M
