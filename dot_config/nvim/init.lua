81b5ba611264c04f25df73490fd7460b2877c7d8
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-03 11:26:41 +0600

    refactor: move nvim option to options file

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -34,17 +34,6 @@ dofile(vim.g.base46_cache .. "statusline")
 
 require("nvchad.autocmds")
 
--- custom settings
-vim.opt.clipboard = ""
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
-
 vim.schedule(function()
 	require("mappings")
 end)
