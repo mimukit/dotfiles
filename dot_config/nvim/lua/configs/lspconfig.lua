683ce8fd562540c1e00bad68a04bf0fb5b0361bd
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-03 15:24:16 +0600

    chore: update lsp config in nvim

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,24 +1,37 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
--- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local servers = {
+	"bashls",
+	"cssls",
+	"dockerls",
+	"docker_compose_language_service",
+	"eslint",
+	"emmet_ls",
+	"html",
+	"jsonls",
+	"marksman",
+	"mdx_analyzer",
+	"phpactor",
+	"postgres_lsp",
+	"pyright",
+	"sqls",
+	"somesass_ls",
+	"tailwindcss",
+	"taplo",
+	"tsserver",
+	"typos_lsp",
+	"yamlls",
+}
+local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
-
--- configuring single server, example: typescript
--- lspconfig.tsserver.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
