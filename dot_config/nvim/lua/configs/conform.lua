d2df4b00bf1880c8db9c7ca3a8f4251a35b94bbe
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-03 15:14:32 +0600

    chore: update conform config for nvim

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -1,15 +1,25 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	css = { "prettier" },
+	graphql = { "prettier" },
+	html = { "prettier" },
+	javascript = { "prettier" },
+	javascriptreact = { "prettier" },
+	json = { "prettier" },
+	liquid = { "prettier" },
+	lua = { "stylua" },
+	markdown = { "prettier" },
+	python = { "isort", "black" },
+	svelte = { "prettier" },
+	typescript = { "prettier" },
+	typescriptreact = { "prettier" },
+	yaml = { "prettier" },
+	formatters_by_ft = {},
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 1000,
+		lsp_fallback = true,
+	},
 }
 
 return options
