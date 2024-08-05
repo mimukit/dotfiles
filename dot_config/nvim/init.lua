be3b868bd1f85b214a3be0bc6b29322da5a635cb
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-05 12:57:44 +0600

    feat: enable mini plugin modules in nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -895,20 +895,59 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- Animate common Neovim actions
+      require('mini.animate').setup()
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- Comment lines
+      --
+      -- Examples:
+      -- - gc - Toggle comment (like `gcip` - comment inner paragraph)
+      -- - gcc - Toggle comment on current lines
+      -- - dgc - Delete whole comment block
+      require('mini.comment').setup()
+
+      -- Enable basic improvements
+      require('mini.basics').setup {
+        -- Options. Set to `false` to disable.
+        options = {
+          -- Basic options ('number', 'ignorecase', and many more)
+          basic = true,
+
+          -- Extra UI features ('winblend', 'cmdheight=0', ...)
+          extra_ui = false,
+
+          -- Presets for window borders ('single', 'double', ...)
+          win_borders = 'dot',
+        },
+
+        -- Mappings. Set to `false` to disable.
+        mappings = {
+          -- Basic mappings (better 'jk', save with Ctrl+S, ...)
+          basic = true,
+
+          -- Prefix for mappings that toggle common options ('wrap', 'spell', ...).
+          -- Supply empty string to not create these mappings.
+          option_toggle_prefix = [[\]],
+
+          -- Window navigation with <C-hjkl>, resize with <C-arrow>
+          windows = true,
+
+          -- Move cursor in Insert, Command, and Terminal mode with <M-hjkl>
+          move_with_alt = true,
+        },
+
+        -- Autocommands. Set to `false` to disable
+        autocommands = {
+          -- Basic autocommands (highlight on yank, start Insert in terminal, ...)
+          basic = true,
+
+          -- Set 'relativenumber' only in linewise and blockwise Visual mode
+          relnum_in_visual_mode = false,
+        },
+
+        -- Whether to disable showing non-error feedback
+        silent = false,
+      }
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
