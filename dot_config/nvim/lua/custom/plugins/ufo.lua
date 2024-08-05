b923b31921a5a1dc3998b90fe74f2d1ed6eb2370
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-05 23:15:32 +0600

    feat(nvim): add ufo fold unfold plugin

---
+++ dot_config/nvim/lua/custom/plugins/ufo.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = 'kevinhwang91/promise-async',
+
+    config = function()
+      require('ufo').setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'lsp', 'indent' }
+        end,
+      }
+
+      -- Custom keymaps
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all UFO folds' })
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all UFO folds' })
+      vim.keymap.set('n', 'zK', function()
+        local winid = require('ufo').peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, { desc = 'Peek Fold' })
+    end,
+  },
+}
