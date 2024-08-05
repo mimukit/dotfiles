47a696c96028c9e0576ade8a60541f8c222c2c49
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-05 12:58:21 +0600

    fix: update fidget plugin config to make it faster

--- dot_config/nvim/lua/custom/plugins/fidget.lua
+++ dot_config/nvim/lua/custom/plugins/fidget.lua
@@ -6,8 +6,8 @@ return {
       progress = {
         poll_rate = 0, -- How and when to poll for progress messages
         suppress_on_insert = false, -- Suppress new messages while in insert mode
-        ignore_done_already = false, -- Ignore new tasks that are already complete
-        ignore_empty_message = false, -- Ignore new tasks that don't contain a message
+        ignore_done_already = true, -- Ignore new tasks that are already complete
+        ignore_empty_message = true, -- Ignore new tasks that don't contain a message
         -- Clear notification group when LSP server detaches
         clear_on_detach = function(client_id)
           local client = vim.lsp.get_client_by_id(client_id)
@@ -22,12 +22,12 @@ return {
         -- Options related to how LSP progress messages are displayed as notifications
         display = {
           render_limit = 16, -- How many LSP messages to show at once
-          done_ttl = 3, -- How long a message should persist after completion
+          done_ttl = 1, -- How long a message should persist after completion
           done_icon = '✔', -- Icon shown when all LSP progress tasks are complete
           done_style = 'Constant', -- Highlight group for completed LSP tasks
           progress_ttl = math.huge, -- How long a message should persist when in progress
           -- Icon shown when LSP progress tasks are in progress
-          progress_icon = { pattern = 'dots', period = 1 },
+          progress_icon = { pattern = 'dots', period = 3 },
           -- Highlight group for in-progress LSP tasks
           progress_style = 'WarningMsg',
           group_style = 'Title', -- Highlight group for group name (LSP server name)
@@ -58,7 +58,7 @@ return {
 
       -- Options related to notification subsystem
       notification = {
-        poll_rate = 10, -- How frequently to update and render notifications
+        poll_rate = 100, -- How frequently to update and render notifications
         filter = vim.log.levels.INFO, -- Minimum notifications level
         history_size = 128, -- Number of removed messages to retain in history
         override_vim_notify = false, -- Automatically override vim.notify() with Fidget
