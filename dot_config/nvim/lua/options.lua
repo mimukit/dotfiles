ce64129520555b40c13f835d01672c496c24c467
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-05 23:15:32 +0600

    feat(nvim): add ufo fold unfold plugin

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -77,3 +77,9 @@ vim.opt.wrap = false
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
+
+-- Ufo plugin - fold/unfold manager
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
