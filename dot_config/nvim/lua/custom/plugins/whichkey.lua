a50074113c98e79c7ea7d675200940fe924703ba
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-05 20:03:27 +0600

    refactor: move plugins from init file to separate files in nvim

---
+++ dot_config/nvim/lua/custom/plugins/whichkey.lua
@@ -0,0 +1,20 @@
+return {
+  { -- Useful plugin to show you pending keybinds.
+    'folke/which-key.nvim',
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    config = function() -- This is the function that runs, AFTER loading
+      require('which-key').setup()
+
+      -- Document existing key chains
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>W', group = '[W]orkspace' },
+      }
+    end,
+  },
+}
