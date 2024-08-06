32a5914e780b2c38764982257ec9aa9c5f48ee10
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-06 20:15:35 +0600

    chore(nvim): delete render markdown plugin

--- dot_config/nvim/lua/custom/plugins/rendermarkdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    event = 'VeryLazy',
-    ft = 'markdown',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    opts = {},
-  },
-}
