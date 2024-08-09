b4211a224a2dbcb9b463f6538137c36f4a3ea7e9
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-09 11:17:25 +0600

    chore(nvim): add tab settings

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -78,8 +78,13 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
--- Fold unfold settins
+-- Fold unfold settings
 vim.o.foldcolumn = '0' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- Tab settings
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
