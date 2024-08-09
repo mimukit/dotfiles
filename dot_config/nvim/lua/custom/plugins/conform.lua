6d852480e3bbab426567825060e6673c2de49ec2
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-09 12:45:16 +0600

    fix(nvim): disable timeout for conform plugin

--- dot_config/nvim/lua/custom/plugins/conform.lua
+++ dot_config/nvim/lua/custom/plugins/conform.lua
@@ -15,13 +15,13 @@ return {
     },
     opts = {
       notify_on_error = true,
+      async = true,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
-          timeout_ms = 1000,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
