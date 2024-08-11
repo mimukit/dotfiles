32bebc2fee5bd62769e2c034d59d19fb47f594f1
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-11 18:05:56 +0600

    fix(nvim): update formatting

--- dot_config/nvim/lua/custom/plugins/lsconfig.lua
+++ dot_config/nvim/lua/custom/plugins/lsconfig.lua
@@ -20,14 +20,14 @@ return {
       'nvim-tree/nvim-web-devicons', -- optional
     },
     config = function()
-      require('lspsaga').setup {
+      require('lspsaga').setup({
         lightbulb = {
           enable = false,
         },
         outline = {
           win_width = 60,
         },
-      }
+      })
     end,
   },
   {
@@ -149,7 +149,7 @@ return {
               group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
               callback = function(event2)
                 vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                vim.api.nvim_clear_autocmds({ group = 'kickstart-lsp-highlight', buffer = event2.buf })
               end,
             })
           end
@@ -160,7 +160,7 @@ return {
           -- This may be unwanted, since they displace some of your code
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
             map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
             end, '[T]oggle Inlay [H]ints')
           end
         end,
@@ -244,9 +244,9 @@ return {
         'markdownlint',
         'pylint',
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
-      require('mason-lspconfig').setup {
+      require('mason-lspconfig').setup({
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
@@ -257,8 +257,7 @@ return {
             require('lspconfig')[server_name].setup(server)
           end,
         },
-      }
+      })
     end,
   },
 }
-
