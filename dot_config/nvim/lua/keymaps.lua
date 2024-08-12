9bcb77af9808861c0f95996e8341af6dae16ccc1
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-12 18:38:38 +0600

    fix(nvim): update code formatting

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -38,7 +38,7 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>tq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 vim.keymap.set('n', '<Esc>', '<cmd>noh<CR>', { desc = 'Clear search highlights' })
 vim.keymap.set('n', ';', ':', { desc = 'CMD enter command mode' })
@@ -87,7 +87,7 @@ vim.keymap.set('n', '<leader>qS', function()
   require('persistence').select()
 end, { desc = 'Select a session to load' })
 vim.keymap.set('n', '<leader>ql', function()
-  require('persistence').load { last = true }
+  require('persistence').load({ last = true })
 end, { desc = 'Load the last session' })
 vim.keymap.set('n', '<leader>qd', function()
   require('persistence').stop()
