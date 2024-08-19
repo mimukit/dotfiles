3b2304a9cc4f15f9ccd0c71175e41a5fb9f34587
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-19 19:55:20 +0600

    chore(nvim): customize dashboard

---
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "goolord/alpha-nvim",
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+      local logo = [[
+                                                                           
+            ████ ██████           █████      ██                     
+            ███████████             █████                             
+            █████████ ███████████████████ ███   ███████████   
+          █████████  ███    █████████████ █████ ██████████████   
+          █████████ ██████████ █████████ █████ █████ ████ █████   
+        ███████████ ███    ███ █████████ █████ █████ ████ █████  
+      ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+      ]]
+
+      dashboard.section.header.val = vim.split(logo, "\n")
+      -- stylua: ignore
+      dashboard.section.buttons.val = {
+          dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
+          dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
+          dashboard.button("r", " " .. " Recent files",    "<cmd>  Telescope oldfiles <cr>"),
+          dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+          dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
+          dashboard.button("t", "☷ " .. " Mason",           "<cmd> Mason <cr>"),
+          dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+      }
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+      dashboard.opts.layout[1].val = 8
+      return dashboard
+    end,
+  },
+}
