4edaa039e4cb70c33894806df436d8f5456a55af
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-21 14:17:54 +0600

    chore(nvim): move telescope file browser keymaps to config file

--- dot_config/nvim/lua/plugins/telescopefilebrowser.lua
+++ dot_config/nvim/lua/plugins/telescopefilebrowser.lua
@@ -5,6 +5,13 @@ return {
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").load_extension("file_browser")
+
+      vim.keymap.set("n", "<leader>sB", function()
+        require("telescope").extensions.file_browser.file_browser({
+          path = vim.fn.expand("%:p:h"),
+          select_buffer = true,
+        })
+      end, { desc = "Telescope file browser" })
     end,
   },
 }
