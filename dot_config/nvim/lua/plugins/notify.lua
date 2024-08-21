95ef4f55c8a5557c7b26e058dfa591b812d173fd
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-21 14:17:17 +0600

    chore(nvim): update notify plugin window style

---
+++ dot_config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      render = "minimal", -- default, compact, minimal, simple
+      stages = "fade_in_slide_out", -- fade, fade_in_slide_out, slide, static
+      timeout = 500,
+      top_down = true,
+    },
+  },
+}
