19d00b1ecfe813647174660f657298ed3ff9ee20
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-21 14:16:34 +0600

    chore(nvim): organize keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,8 @@
--- Keymaps are automatically loaded on the VeryLazy event
+-- INFO: Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
+-- General improvements
 vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
 
 vim.keymap.set("n", "J", "mzJ`z", { desc = "Concat next line" })
@@ -14,17 +10,20 @@ vim.keymap.set("n", "J", "mzJ`z", { desc = "Concat next line" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "Go to next search item and keep cursor in center of the screen" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to previous search item and keep cursor in center of the screen" })
 
+-- Buffer management
 vim.keymap.set("n", "<leader>bv", "ggVG", { desc = "Select whole file" })
 vim.keymap.set("n", "<leader>by", "<cmd>%y+<CR>", { desc = "Copy whole file" })
 
--- vim.keymap.set({ "n", "v" }, "<leader>w", "<cmd> w <cr>", { desc = "Save current buffer" })
 -- Move cursor in insert mode
 vim.keymap.set("i", "<M-j>", "<Down>", { desc = "Move cursor down" })
 vim.keymap.set("i", "<M-k>", "<Up>", { desc = "Move cursor up" })
 vim.keymap.set("i", "<M-h>", "<Left>", { desc = "Move cursor left" })
 vim.keymap.set("i", "<M-l>", "<Right>", { desc = "Move cursor right" })
 
--- Navigator
+-- Exit terminal mode in the builtin terminal
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Navigator keymaps
 vim.keymap.set({ "n", "t" }, "<C-h>", "<CMD>NavigatorLeft<CR>")
 vim.keymap.set({ "n", "t" }, "<C-l>", "<CMD>NavigatorRight<CR>")
 vim.keymap.set({ "n", "t" }, "<C-k>", "<CMD>NavigatorUp<CR>")
@@ -37,11 +36,3 @@ vim.keymap.set("n", "<leader>uv", "<CMD>vsplit<CR>", { desc = "Split pane vertic
 vim.keymap.set({ "n", "v" }, "<leader>cy", [["+y]], { desc = "Copy to system clipboard" })
 vim.keymap.set("n", "<leader>cY", [["+Y]], { desc = "Copy current line to clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>cp", [["+p]], { desc = "Paste from system clipboard" })
-
--- Telescope extensions
-vim.keymap.set("n", "<leader>sB", function()
-  require("telescope").extensions.file_browser.file_browser({
-    path = vim.fn.expand("%:p:h"),
-    select_buffer = true,
-  })
-end, { desc = "Telescope file browser" })
