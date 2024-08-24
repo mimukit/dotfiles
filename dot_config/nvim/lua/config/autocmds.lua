9db64e565a778d83543afb96d5eb4493b007a845
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-24 16:08:06 +0600

    feat(nvim): add auto command to customize lsp info window border

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
--- Autocmds are automatically loaded on the VeryLazy event
+-- INFO: Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+
 -- Add any additional autocmds here
+
+-- INFO: Customize LSP Info UI window border
+local function set_lsp_info_ui_border()
+  require("lspconfig.ui.windows").default_options.border = "rounded"
+end
+
+-- Create an auto command to set the border when LSP attaches
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = set_lsp_info_ui_border,
+})
