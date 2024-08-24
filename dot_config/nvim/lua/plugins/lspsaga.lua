bb7c4d25562d03b68dd0edc169e53ee5597e4d30
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-24 22:35:48 +0600

    chore(nvim): add new keymaps & sort order

--- dot_config/nvim/lua/plugins/lspsaga.lua
+++ dot_config/nvim/lua/plugins/lspsaga.lua
@@ -19,12 +19,14 @@ return {
     -- stylua: ignore
     keys = {
       { "<leader>cga", "<cmd>Lspsaga code_action<cr>", desc = "Lspsaga code action" },
+      { "<leader>cgd", "<cmd>Lspsaga goto_definition<cr>", desc = "Lapsaga goto definition" },
+      { "<leader>cgf", "<cmd>Lspsaga finder<cr>", desc = "Lspsaga finder" },
       { "<leader>cgo", "<cmd>Lspsaga outline<cr>", desc = "Lspsaga outline" },
       { "<leader>cgr", "<cmd>Lspsaga rename<cr>", desc = "Lspsaga rename" },
-      { "<leader>cgf", "<cmd>Lspsaga finder<cr>", desc = "Lspsaga finder" },
-      { "<leader>cgK", "<cmd>Lspsaga hover_doc<cr>", desc = "Lspsaga hover doc" },
-      { "<leader>cgd", "<cmd>Lspsaga goto_definition<cr>", desc = "Lapsaga goto definition" },
       { "<leader>cgw", "<cmd>Lspsaga show_workspace_diagnostics<cr>", desc = "Lapsaga show workspace diagnostics",},
+      { "<leader>cgK", "<cmd>Lspsaga hover_doc<cr>", desc = "Lspsaga hover doc" },
+
+      { "<leader>co", "<cmd>Lspsaga outline<cr>", desc = "Lspsaga outline" },
     },
   },
 }
