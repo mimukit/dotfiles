8300c6c6f18dfc91e88bddc9bec90baf8748fbdf
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-25 01:07:58 +0600

    chore(nvim): revert mac only treesitter config

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,50 +1,39 @@
-local function is_macos()
-  local os_type = vim.loop.os_uname().sysname
-  return os_type == "Darwin"
-end
-
-local treesitter_list = {}
-
-if is_macos() then
-  treesitter_list = {
-    "bash",
-    "c",
-    "css",
-    "csv",
-    "dockerfile",
-    "fish",
-    "gitignore",
-    "html",
-    "javascript",
-    "jsdoc",
-    "json",
-    "kdl",
-    "lua",
-    "luadoc",
-    "markdown",
-    "markdown_inline",
-    "php",
-    "printf",
-    "query",
-    "regex",
-    "robots",
-    "scss",
-    "ssh_config",
-    "tmux",
-    "toml",
-    "tsx",
-    "typescript",
-    "vim",
-    "vimdoc",
-    "yaml",
-  }
-end
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      ensure_installed = treesitter_list,
+      ensure_installed = {
+        "bash",
+        "c",
+        "css",
+        "csv",
+        "dockerfile",
+        "fish",
+        "gitignore",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "kdl",
+        "lua",
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "php",
+        "printf",
+        "query",
+        "regex",
+        "robots",
+        "scss",
+        "ssh_config",
+        "tmux",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
