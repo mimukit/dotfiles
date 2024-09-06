85b5d2333b89399213d53f0a8674e5f36a53823e
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-09-06 15:46:01 +0600

    fix(nvim): update plugin load events

--- dot_config/nvim/lua/plugins/notify.lua
+++ dot_config/nvim/lua/plugins/notify.lua
@@ -1,6 +1,7 @@
 return {
   {
     "rcarriga/nvim-notify",
+    event = "VeryLazy",
     opts = {
       render = "minimal", -- default, compact, minimal, simple
       stages = "fade_in_slide_out", -- fade, fade_in_slide_out, slide, static
