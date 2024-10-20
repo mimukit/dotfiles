7ea22145a699909b339e15b288ba36cba1bc9f37
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-10-20 16:05:54 +0600

    chore(nvim): disable markdown render plugins

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@
 return {
   { "folke/tokyonight.nvim", enabled = false },
   { "MeanderingProgrammer/markdown.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
