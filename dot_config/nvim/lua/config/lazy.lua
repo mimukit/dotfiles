77c0b99cb2013cd9d595b36331fd0cde5498b05a
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-11-16 11:34:56 +0600

    fix(neovim): update lazyvim extra plugin path

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -29,8 +29,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.alpha" },
 
     -- add lazyvim extra plugins
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
