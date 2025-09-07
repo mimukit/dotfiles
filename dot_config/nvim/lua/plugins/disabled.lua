e55af7bb314e11e3b79413e21058da9c13f46359
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-09-07 15:15:02 +0600

    feat(nvim): disable bufferline plugin

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 -- disabled lazyvim built-in plugins
 return {
+  { "akinsho/bufferline.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "MeanderingProgrammer/markdown.nvim", enabled = false },
   { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
