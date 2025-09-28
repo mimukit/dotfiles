46fb95e9c527d1a85ddaa996d793a063400530ad
Mukitul Islam Mukit <hellomukit@gmail.com>
2025-09-28 12:43:53 +0600

    refactor(nvim): update package name for mason and mini plugins

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,7 +1,7 @@
 return {
   -- Collection of various small independent plugins/modules
   {
-    "echasnovski/mini.nvim",
+    "nvim-mini/mini.nvim",
     event = "VeryLazy",
     config = function()
       -- INFO: Enable basic improvements
