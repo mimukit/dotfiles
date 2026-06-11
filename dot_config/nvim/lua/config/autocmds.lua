539fbfa25c4920049503f9fec763a1498a7816a2
Mukitul Islam Mukit <hellomukit@gmail.com>
2026-06-11 15:49:21 +0600

    feat(nvim): configure Nvim with keymaps, options, and diagnostics
    This commit introduces several configuration enhancements to Nvim,
    including new key mappings, adjusted options, and diagnostic settings.
    
    - Update LazyVim lock file commit hash.
    - Disable diagnostics for .env files to reduce noise.
    - Add general and navigator keymaps for improved navigation and command
    entry.
    - Configure various Nvim options for enhanced editing experience,
    including Nerd Font support, clipboard separation, and indentation
    settings.
    - Adjust fold and tab settings for better code readability.

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- INFO: Disable diagnostics for .env files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.env*",
+  callback = function()
+    vim.diagnostic.enable(false)
+  end,
+})
--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- General improvements
+vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Go to next search item and keep cursor in center of the screen" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to previous search item and keep cursor in center of the screen" })
+
+-- Navigator keymaps
+vim.keymap.set({ "n", "t" }, "<C-h>", "<CMD>NavigatorLeft<CR>")
+vim.keymap.set({ "n", "t" }, "<C-l>", "<CMD>NavigatorRight<CR>")
+vim.keymap.set({ "n", "t" }, "<C-k>", "<CMD>NavigatorUp<CR>")
+vim.keymap.set({ "n", "t" }, "<C-j>", "<CMD>NavigatorDown<CR>")
