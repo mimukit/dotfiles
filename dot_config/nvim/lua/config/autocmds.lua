81f6f02f79e961581b54f18819b08a4b8fc3fdd8
Mukitul Islam Mukit <hellomukit@gmail.com>
2026-06-11 12:52:52 +0600

    chore(nvim): replace custom vim with lazyvim official distro

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,22 +1,8 @@
--- INFO: Autocmds are automatically loaded on the VeryLazy event
+-- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
-
+--
 -- Add any additional autocmds here
-
--- INFO: Customize LSP Info UI window border
-local function set_lsp_info_ui_border()
-  require("lspconfig.ui.windows").default_options.border = "rounded"
-end
-
--- Create an auto command to set the border when LSP attaches
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = set_lsp_info_ui_border,
-})
-
--- INFO: Disable diagnostics for .env files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*.env*",
-  callback = function()
-    vim.diagnostic.enable(false)
-  end,
-})
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
